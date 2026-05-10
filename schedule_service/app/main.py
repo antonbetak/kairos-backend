8fb1bb084466fe9fa76318ea271caf2b0967e626
Anton Betak Licea <anton@Antons-MacBook-Air.local>
2026-05-10 16:52:26 -0600

    Conexión a Postgres

--- schedule_service/app/main.py
+++ schedule_service/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
+from app.database import Base
+from app.database import engine
+
 app = FastAPI(title="Kairos Schedule Service")
+
+
+@app.on_event("startup")
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/health")
