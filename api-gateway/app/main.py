7b821f632272a9c6eb44bf61c0094049eae01d75
Anton Betak Licea <anton@Antons-MacBook-Air.local>
2026-05-10 18:23:42 -0600

    feat: Integración de schedule en Api-gateway

--- api-gateway/app/main.py
+++ api-gateway/app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from fastapi import Depends
 
 from app.dependencies.auth import obtener_usuario_actual
+from app.services.schedule_client import actualizar_horario
+from app.services.schedule_client import crear_horario
+from app.services.schedule_client import eliminar_horario
+from app.services.schedule_client import listar_horarios
+from app.services.schedule_client import obtener_horario
 from app.services.task_client import crear_tarea
 from app.services.task_client import listar_tareas
 
@@ -23,3 +28,37 @@ async def obtener_tareas(usuario=Depends(obtener_usuario_actual)):
 async def crear_nueva_tarea(datos: dict, usuario=Depends(obtener_usuario_actual)):
     id_usuario = usuario["id_usuario"]
     return await crear_tarea(id_usuario, datos)
+
+
+@app.post("/schedule")
+async def crear_nuevo_horario(datos: dict, usuario=Depends(obtener_usuario_actual)):
+    id_usuario = usuario["id_usuario"]
+    return await crear_horario(id_usuario, datos)
+
+
+@app.get("/schedule")
+async def obtener_horarios(usuario=Depends(obtener_usuario_actual)):
+    id_usuario = usuario["id_usuario"]
+    return await listar_horarios(id_usuario)
+
+
+@app.get("/schedule/{schedule_id}")
+async def obtener_horario_por_id(schedule_id: str, usuario=Depends(obtener_usuario_actual)):
+    id_usuario = usuario["id_usuario"]
+    return await obtener_horario(id_usuario, schedule_id)
+
+
+@app.patch("/schedule/{schedule_id}")
+async def actualizar_horario_por_id(
+    schedule_id: str,
+    datos: dict,
+    usuario=Depends(obtener_usuario_actual),
+):
+    id_usuario = usuario["id_usuario"]
+    return await actualizar_horario(id_usuario, schedule_id, datos)
+
+
+@app.delete("/schedule/{schedule_id}")
+async def eliminar_horario_por_id(schedule_id: str, usuario=Depends(obtener_usuario_actual)):
+    id_usuario = usuario["id_usuario"]
+    return await eliminar_horario(id_usuario, schedule_id)
