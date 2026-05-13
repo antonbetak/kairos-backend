d11baa3d3c15dfa42608d830221a4c5bde4a0f07
Anton Betak Licea <anton@Antons-MacBook-Air.local>
2026-05-12 23:26:58 -0600

    feat:Implementación de schemas de notificaciones

---
+++ notifications_service/app/schemas.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+from pydantic import ConfigDict
+
+
+class NotificacionRespuesta(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id_notificacion: UUID
+    id_usuario: UUID
+    titulo: str
+    mensaje: str
+    tipo: str
+    leida: bool
+    fecha_creacion: datetime
+    fecha_lectura: datetime | None
