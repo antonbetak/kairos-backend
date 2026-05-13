6c932c3d62ea8779832fb3cbb0fea23d90885dfd
Anton Betak Licea <anton@Antons-MacBook-Air.local>
2026-05-12 20:39:56 -0600

    Creación de base de stats service

--- stats_service/app/main.py
+++ stats_service/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
+from app.db import Base
+from app.db import engine
+
 app = FastAPI(title="Kairos Stats Service")
+
+
+@app.on_event("startup")
+def iniciar_base_de_datos():
+    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/health")
